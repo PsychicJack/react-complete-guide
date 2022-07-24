e88bf16c025a330e18cfcdf9dd5cf3154b8bdee7
Perica Milosevic <supremeruler98@gmail.com>
2022-07-24 15:59:54 +0200

    idk

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewEX/NewExpense";
 
 function App() {
 
     return (
         <div>
+            <NewExpense></NewExpense>
             <Expenses></Expenses>
         </div>
     );
